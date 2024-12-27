f1652215d7b511db0ba624d6ba0b92f9abc2ec22
syandhilakarno <if23.syandhilakarno@mhs.ubpkarawang.ac.id>
2024-12-27 11:31:33 +0700

    Add files via upload
    Signed-off-by: syandhilakarno <if23.syandhilakarno@mhs.ubpkarawang.ac.id>

---
+++ FPp/about.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Company CV</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/cyborg/bootstrap.min.css"
+      integrity="sha384-nEnU7Ae+3lD52AK+RGNzgieBWMnEfgTbRHIwEvp1XXPdqdO6uLTd/NwXbzboqjc2" crossorigin="anonymous">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"href="index1.css">
+      <style>
+        *{
+          font-weight: bold;
+        }
+        body {
+        background-color: #e8e8e8;
+        align-items: center;
+        font-family: Arial, sans-serif;
+        }
+        .navbar {
+        position: fixed;
+        top: 0;
+        width: 100%;
+        z-index: 2000;
+        background-color: #4B569F;
+        }
+
+        .navbar-brand,
+        .navbar-nav .nav-link {
+        color: #ffffff !important;
+        font-weight: bold;
+        }
+
+        .nav-link:hover {
+        color: black !important;
+        }
+        .navbar-brand:hover {
+        color: black !important;
+        }
+        .container {
+          width: 80%;
+          margin: 0 auto;
+          margin-top:80px;
+          padding: 20px;
+          background: white;
+          box-shadow: 0 2px 5px rgba(149, 136, 136, 0.1);
+        }
+        header {
+          text-align: center;
+          margin-bottom: 40px;
+        }
+        header h1 {
+          font-weight: bold;
+          margin: 0;
+          color:#4B569F;
+        }
+        .section {
+          margin-bottom: 20px;
+        }
+        .section h2 {
+          font-weight: bold;
+          color: #4B569F;
+          border-bottom: 2px solid #4B569F;
+          padding-bottom: 5px;
+        }
+        .section p {
+          color:#4B569F;
+        }
+        ul {
+          list-style: none;
+          padding: 0;
+        }
+        ul li {
+          color:#4B569F;
+          margin-bottom: 10px;
+        }
+        .social-icons a {
+        color: #4B569F;
+        font-size: 18px;
+        }
+
+        .social-icons a:hover {
+        color: #3E4687;
+        }
+      </style>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <a class="navbar-brand" href="index.php"><i class="fas fa-bus"></i> BUROQ TRANSPORT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  </head>
+      <div class="container">
+          <header>
+              <h1>BUROQ TRANSPORT</h1>
+              <p></p>
+          </header>
+          <div class="section">
+              <h2>About Us</h2>
+              <p></p>
+          </div>
+          <div class="section">
+              <h2>Melayani</h2>
+              <ul>
+                  <li>Jemput Karwayan</li>
+                  <li>Rental mobil</li>
+                  <li>Ziarah & Tour</li>
+              </ul>
+          </div>
+          <div class="section">
+              <h2>Our Team</h2>
+              <p>Meet the talented individuals behind our success:</p>
+              <ul>
+                  <li><strong>John Doe:</strong> CEO</li>
+              </ul>
+          </div>
+          <div class="section">
+              <h2>Hubungi</h2>
+              <p>Email: buroqtransport@gmail.com</p>
+              <p>No Handphone : 081 320 075 917</p>
+              <p>Alamat : Babakan Bandung RT.03/20 kel.Adarsa Barat kec.Karawang Barat</p>
+          </div>
+      </div>
+  </body>
+  </html>
+  
